582ae963245583d7a3c7431578f38314f6bc0b1e
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2025-12-11 17:13:39 +0330

    fix bot token for admin panel

--- AdminPanel/config.py
+++ AdminPanel/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CONF = {
+    "BOT_TOKEN": os.getenv("BOT_TOKEN"),
     "ADMIN_BOT_TOKEN": os.getenv("ADMIN_BOT_TOKEN"),
     "MONGO_URL": os.getenv("MONGODB_URL", "mongodb://localhost:27017"),
     "DB_NAME": os.getenv("DB_NAME", "act_cast_db"),
@@ -12,12 +13,11 @@ CONF = {
 }
 
 # چک کردن مقادیر حیاتی
-if not CONF["ADMIN_BOT_TOKEN"] or not CONF["STORAGE_CHANNEL_ID"]:
+if not CONF["ADMIN_BOT_TOKEN"] or not CONF["STORAGE_CHANNEL_ID"] or not CONF["BOT_TOKEN"]:
     raise ValueError(
-        "ADMIN_BOT_TOKEN or STORAGE_CHANNEL_ID is missing in .env")
+        "ADMIN_BOT_TOKEN or STORAGE_CHANNEL_ID or BOT_TOKEN is missing in .env")
+
 
 # تابع چک کردن ادمین
-
-
 def is_admin(user_id: int) -> bool:
     return user_id in CONF["ADMIN_IDS"]
